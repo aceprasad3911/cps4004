fa29e3fcdffb4d0962f87ab5eeb6a825577f60f4
aceprasad3911 <2306629@live.stmarys.ac.uk>
2024-02-09 09:59:06 +0000

    Employee Table Added

---
+++ Week2/hr.sql
@@ -0,0 +1,11 @@
+.open hr.db
+CREATE TABLE employee (
+    employee_id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT UNIQUE,
+    phone_number TEXT,
+    hire_date DATE,
+    job_title TEXT,
+    salary REAL
+);

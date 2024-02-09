80b29e841503fb6b08bca0799ba709a9541386fa
aceprasad3911 <2306629@live.stmarys.ac.uk>
2024-02-09 12:01:37 +0000

    all tables with associated components and data types added

--- Week2/hr.sql
+++ Week2/hr.sql
@@ -4,6 +4,7 @@
 /* sqlite3 (dictates what type of language + program is used*/
 /*.open in hr.db (hr.db is associated database in week 2 pycharm folder)*/
 /* copy and past relevant commands into terminal*/
+/* to delete table, enter "DROP TABLE -TABLE NAME-;"*/
 CREATE TABLE employee (
     employee_id INTEGER PRIMARY KEY,
     first_name TEXT,
@@ -13,4 +14,35 @@ CREATE TABLE employee (
     hire_date DATE,
     job_title TEXT,
     salary REAL
+);
+
+CREATE TABLE department (
+    department_id INTEGER PRIMARY KEY,
+    department_name TEXT UNIQUE,
+    manager_id INTEGER,
+    location_id INTEGER
+);
+
+CREATE TABLE task (
+    task_id INTEGER PRIMARY KEY,
+    task_name TEXT,
+    description TEXT
+);
+
+CREATE TABLE employee_task (
+    employee_id INTEGER,
+    task_id INTEGER
+);
+
+CREATE TABLE job_title (
+    job_title_id INTEGER PRIMARY KEY,
+    title_name TEXT,
+    description TEXT,
+    base_salary REAL
+);
+
+CREATE TABLE location (
+    city TEXT,
+    state TEXT,
+    country TEXT
 );

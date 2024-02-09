53c76d38c1c92f52ec5e7f6ff09aedddaa571e7f
aceprasad3911 <2306629@live.stmarys.ac.uk>
2024-02-09 11:23:01 +0000

    File nature update, hr.db and hr.sql added and reconfigured so variables are indicated within hr.db

--- Week2/hr.sql
+++ Week2/hr.sql
@@ -1,4 +1,9 @@
-.open hr.db
+/*Enter file path to make file amendments (open PycharmProjects
+  folder in finder, locate file, open more info and copy, paste "where" section*/
+/*Open file in terminal and execute commands after typying following:*/
+/* sqlite3 (dictates what type of language + program is used*/
+/*.open in hr.db (hr.db is associated database in week 2 pycharm folder)*/
+/* copy and past relevant commands into terminal*/
 CREATE TABLE employee (
     employee_id INTEGER PRIMARY KEY,
     first_name TEXT,
